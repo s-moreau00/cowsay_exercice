2bb03ae631281dc0a65cf3488f6428ea8ecc753a
s-moreau00 <moreau.sylvain6969@gmail.com>
2023-11-22 01:10:14 +0100

    cowsay exercice

---
+++ index.js
@@ -0,0 +1,13 @@
+const {info} = require ("./information");
+const cowsay = require ("cowsay");
+
+// console.log(`salut je m'appelle ${info.name}`);
+// console.log(cowsay.say({
+// }));
+
+
+
+console.log(cowsay.say({
+    text : `saluuut je m'appelle ${info.name}`,
+    e : "oO"
+}));
---
+++ information.js
@@ -0,0 +1,9 @@
+const info = {
+    name : "Sylvain", college : "wcs"
+};
+
+// console.log(info);
+
+module.exports =  {
+    info
+}
